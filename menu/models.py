b53feefee3ed25bf338b61caa473194ae27e1efb
nepa27 <nepalek@mail.ru>
2024-02-09 14:07:22 +0300

    Ready test exercise

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+LIMIT_LENGTH: int = 20
+
+
+class Menu(models.Model):
+    title = models.CharField(
+        max_length=LIMIT_LENGTH,
+        unique=True,
+        verbose_name='Название'
+    )
+    slug = models.SlugField(
+        max_length=LIMIT_LENGTH,
+        verbose_name='Слаг'
+    )
+
+    class Meta:
+        verbose_name = 'Меню'
+        verbose_name_plural = 'меню'
+
+    def __str__(self):
+        return self.title
+
+
+class Item(models.Model):
+    title = models.CharField(
+        max_length=LIMIT_LENGTH,
+        verbose_name='Название элемента'
+    )
+    slug = models.SlugField(
+        max_length=LIMIT_LENGTH,
+        verbose_name='Слаг'
+    )
+    menu = models.ForeignKey(
+        Menu,
+        blank=True,
+        related_name='items',
+        on_delete=models.CASCADE
+    )
+    parent = models.ForeignKey(
+        'self',
+        blank=True,
+        null=True,
+        related_name='childrens',
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        verbose_name = 'Элемент меню'
+        verbose_name_plural = 'элементы меню'
+
+    def __str__(self):
+        return self.title
--- test_project/settings.py
+++ test_project/settings.py
@@ -88,6 +88,6 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static', ]
+# STATICFILES_DIRS = [BASE_DIR / 'static', ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
